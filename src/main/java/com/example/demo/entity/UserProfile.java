66dec2cb74c84e1e7b9d261da608c5fb92f5b31a
bala-amypo <bala@amypo.in>
2025-12-21 06:15:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -8,7 +8,7 @@ public class UserProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")   // maps to DB column correctly
+    @Column(name = "user_id")   // IMPORTANT !!!
     private Long id;
 
     @Column(name = "full_name")
@@ -24,78 +24,74 @@ public class UserProfile {
     @Column(name = "monthly_income")
     private Double monthlyIncome;
 
-    public UserProfile() {}
-
-    // ----------------- GETTERS -----------------
+    // ---------- GETTERS & SETTERS ----------
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFullName() {
         return fullName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public Double getMonthlyIncome() {
-        return monthlyIncome;
-    }
-
-    // ----------------- SETTERS -----------------
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getState() {
+        return state;
     }
 
     public void setState(String state) {
         this.state = state;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public Double getMonthlyIncome() {
+        return monthlyIncome;
     }
 
     public void setMonthlyIncome(Double monthlyIncome) {
