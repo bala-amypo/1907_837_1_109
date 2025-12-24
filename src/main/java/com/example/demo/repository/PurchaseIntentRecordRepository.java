9565c66ad9f2ee52a7f636e363a681caf79f39ac
bala-amypo <bala@amypo.in>
2025-12-24 17:46:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/PurchaseIntentRecordRepository.java
+++ src/main/java/com/example/demo/repository/PurchaseIntentRecordRepository.java
@@ -7,4 +7,14 @@
 // @Repository
 // public interface PurchaseIntentRecordRepository extends JpaRepository<PurchaseIntentRecord, Long> {
 //     List<PurchaseIntentRecord> findByUserId(Long userId);
+//}
+
+package com.example.demo.repository;
+
+import com.example.demo.entity.PurchaseIntentRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PurchaseIntentRecordRepository extends JpaRepository<PurchaseIntentRecord, Long> {
+    List<PurchaseIntentRecord> findByUserId(Long userId);
 }
